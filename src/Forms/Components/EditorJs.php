6a331a4f2163efefcfff311cfe8194325d9a7f86
Roman Borkunov <r.borkunov@liga.net>
2024-08-20 01:24:58 +0300

    Update namespace in EditorJs.php
    Changed the namespace for the InteractsWithTools trait to Broqit\FilamentEditorJs to ensure proper class loading. This update resolves issues related to the incorrect namespace reference.

--- src/Forms/Components/EditorJs.php
+++ src/Forms/Components/EditorJs.php
@@ -9,7 +9,7 @@ use Filament\Forms\Components\Concerns\HasFileAttachments;
 use Filament\Forms\Components\Concerns\HasPlaceholder;
 use Filament\Forms\Components\Contracts\HasFileAttachments as HasFileAttachmentsContract;
 use Filament\Forms\Components\Field;
-use FilamentEditorJs\Forms\Components\Concerns\InteractsWithTools;
+use Broqit\FilamentEditorJs\Forms\Components\Concerns\InteractsWithTools;
 
 class EditorJs extends Field implements HasFileAttachmentsContract
 {
